7408548f5f5a8f7c2eee163de082581b95349ac8
Lakshay Sharma <laksharma147@gmail.com>
2026-07-20 16:56:37 +0530

    Add: jump-game - submission-7

---
+++ Data Structures & Algorithms/jump-game/submission-7.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for(int i = 0; i<=maxReach && maxReach< nums.length ; i++){
+            maxReach = Math.max(maxReach, i+nums[i]);
+        }
+        return maxReach>=nums.length-1;
+
+    }
+}
